f9d07acad7d588f33f1cf9a5d41c7a39af46ee5f
buenoIago <iagocedup@gmail.com>
2026-06-03 23:19:21 -0300

    Fix: Validação das Regras de Negócio

--- ListaCompras.WebApplication/ModuloCategoria/Apresentacao/CategoriaViewModel.cs
+++ ListaCompras.WebApplication/ModuloCategoria/Apresentacao/CategoriaViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using ListaCompras.WebApplication.ModuloCategoria.Dominio;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,5 +7,14 @@ namespace ListaCompras.WebApplication.ModuloCategoria.Apresentacao;
 public record ListarCategoriasViewModel(
     string Id,
     string Nome,
+    CorCategoria Cor
+);
+
+public record CadastrarCategoriasViewModel(
+    [Required(ErrorMessage = "O campo \"Nome\" deve ser preenchido.")]
+    [StringLength(50, ErrorMessage = "O campo \"Nome\" deve conter no máximo 50 caracteres.")]
+    string Nome,
+
+    [Range(1, int.MaxValue, ErrorMessage = "O campo \"Cor\" deve ser selecionado.")]
     CorCategoria Cor
 );
