2b71c23271d14abd6135ac9f0a3f6a7478c7878f
buenoIago <iagocedup@gmail.com>
2026-06-04 10:43:31 -0300

    fix: definir string vazia e valor padrão para CorCategoria

--- ListaCompras.WebApplication/ModuloCategoria/Dominio/Categoria.cs
+++ ListaCompras.WebApplication/ModuloCategoria/Dominio/Categoria.cs
@@ -4,12 +4,10 @@ namespace ListaCompras.WebApplication.ModuloCategoria.Dominio;
 
 public class Categoria : EntidadeBase<Categoria>
 {
-    public string Nome { get; set; }
-    public CorCategoria Cor { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public CorCategoria Cor { get; set; } = CorCategoria.Nenhuma;
 
-    public Categoria()
-    {
-    }
+    public Categoria() { }
 
     public Categoria(string nome, CorCategoria cor)
     {
@@ -38,9 +36,7 @@ public class Categoria : EntidadeBase<Categoria>
 
     public override void AtualizarDados(Categoria entidadeAtualizada)
     {
-        Categoria categoriaAtualizada = (Categoria)entidadeAtualizada;
-
-        Nome = categoriaAtualizada.Nome;
-        Cor = categoriaAtualizada.Cor;
+        Nome = entidadeAtualizada.Nome;
+        Cor = entidadeAtualizada.Cor;
     }
 }
